2802c4bbca27368ef6eb65778c181915cb158127
ScylaCrozier <102611735+ScylaCrozier@users.noreply.github.com>
2022-12-12 17:54:33 -0500

    update

---
+++ project-4/java.js
@@ -0,0 +1,12 @@
+window.onload = function() {
+    // First, tell JavaScript to remember which DOM element (in this case, a button) to reference (by id)
+    let toggleNav = document.getElementById('toggle-nav');
+
+    // Tell button to do something on click
+    toggleNav.addEventListener('click', function(){
+
+      // toggle the body's class attribute
+      document.body.classList.toggle('nav-open');
+
+    });
+  };
